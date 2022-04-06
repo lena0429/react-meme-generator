03cb5134593a11de7521641ee064d6f0fc2f086b
Lena Hou <lenahou2013@gmail.com>
2022-04-05 20:23:49 -0400

    using useState hook to set up state in the Meme component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Header from './components/Header';
 import Meme from './components/Meme';
 
 function App() {
+  
+
+
+
+
   return (
     <div className="App">
       <Header />
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import memeData from '../memeData.js';
 
 function Meme(){
+
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function handleClick(){
+        const memeArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memeArray.length)
+        console.log(randomNumber)
+        const url = memeArray[randomNumber].url
+        setMemeImage(url)
+
+        // we can also use object destructuring to get the url property from the memeArray
+        // const {url} = memeArray[randomNumber]
+    }
+
     return(
         <main>
-        <form className="form">
+        <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text" 
@@ -18,12 +33,14 @@ function Meme(){
               
               <button 
                   className="form--button"
+                  onClick={handleClick}
               >
                   Get a new meme image 🖼
               </button>
-        </form>
+        </div>
+            <img src={memeImage} alt="" className="meme--image"/>
         </main>
     )
 }
 
-export default Meme
+export default Meme;
